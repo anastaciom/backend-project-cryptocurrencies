d9a712ae7e5c4ff5bd89d0ea4aacc0087b1708af
Anastacio Menezes <anastaciomenezes@outlook.com>
2022-02-23 23:50:21 -0300

    feat: add news API

--- src/controllers/sessions/index.js
+++ src/controllers/sessions/index.js
@@ -1,20 +1,33 @@
-const UserModel = require('../../models/User');
-const api = require('../../config/api')
+const UserModel = require("../../models/User");
+const api = require("../../config/api");
 
 const sessions = async (req, res) => {
-    try {
-        const user = await UserModel.findById(req.userId.id);
-        if (!user) {
-        return res.status(404).json({ error: `User does not exist or is not authenticated` })  
-        } 
-        const { data } = await api.get('markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h')
-
-        return res.status(200).json({ data: data, user: {name: user.name, email: user.email}})
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ error: 'internal error' })
+  try {
+    const user = await UserModel.findById(req.userId.id);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ error: `User does not exist or is not authenticated` });
     }
-}
+    const { data } = await api.get(
+      "markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h"
+    );
 
+    const news = await api.get(
+      `https://newsapi.org/v2/everything?q=cryptocurrency&from=2022-01-24&sortBy=publishedAt&apiKey=${process.env.API_URL_NEWS}`
+    );
 
-module.exports = { sessions }
+    return res
+      .status(200)
+      .json({
+        news: news.data,
+        data: data,
+        user: { name: user.name, email: user.email },
+      });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "internal error" });
+  }
+};
+
+module.exports = { sessions };
