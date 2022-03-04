726007cbc6f00747079a72fb0174e8c242338a91
Anastacio Menezes <anastaciomenezes@outlook.com>
2022-03-04 15:26:17 -0300

    fix: get user data in admin route

--- src/controllers/admin/users.js
+++ src/controllers/admin/users.js
@@ -9,7 +9,7 @@ const allUsers = async (req, res) => {
         .json({ error: `User does not exist or you are not admin` });
     }
 
-    const allUsers = await UserModel.find({}, { _id: 0, email: 1, name: 1 });
+    const allUsers = await UserModel.find({}, { _id: 0, password: 0, updatedAt: 0, __v: 0});
     return res.status(200).json(allUsers);
     
   } catch (error) {
