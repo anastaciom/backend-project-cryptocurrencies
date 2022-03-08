df746a5c670c8708efa0a4d3a27bdefb39087e07
Anastacio Menezes <anastaciomenezes@outlook.com>
2022-03-08 02:36:36 -0300

    fix: variable port in server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const app = require('./app')
-const PORT = process.env.PORT || 3001
+const port = process.env.PORT || 3001
 const mongoDBConnect = require('./database/config')
 mongoDBConnect()
 
-app.listen(PORT, () => {
-    console.log('Server running on port', PORT)
+app.listen(port, () => {
+    console.log('Server running on port', port)
 })
